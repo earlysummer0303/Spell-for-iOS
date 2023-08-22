958f824a7a289cc6c9c5983f2733da543c38ae91
earlysummer0303 <jw9914@naver.com>
2023-08-22 13:50:34 +0900

    .gitignore 생성

---
+++ Spell/SpellApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpellApp.swift
+//  Spell
+//
+//  Created by 황지우2 on 2023/08/22.
+//
+
+import SwiftUI
+
+@main
+struct SpellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
